360709659e29a171456048faf81682c00b6c405b
cafajardop <carlosafp84@hotmail.com>
2020-03-21 21:08:03 -0500

    Ajuste barra horizontal

--- src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
+++ src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
@@ -1,15 +1,31 @@
-import { Component} from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-grafico-barra-horizontal',
   templateUrl: './grafico-barra-horizontal.component.html',
   styleUrls: ['./grafico-barra-horizontal.component.css']
 })
-export class GraficoBarraHorizontalComponent {
+export class GraficoBarraHorizontalComponent implements OnDestroy {
 
-  results: any[];
-  
-  view: any[] = [700, 400];
+  results: any[] = [
+    {
+      "name": "Juego 1",
+      "value": 20
+    },
+    {
+      "name": "Juego 2",
+      "value": 25
+    },
+    {
+      "name": "Juego 3",
+      "value": 15
+    },
+    {
+      "name": "Juego 4",
+      "value": 15
+    }
+  ];
+
 
   // options
   showXAxis = true;
@@ -23,10 +39,29 @@ export class GraficoBarraHorizontalComponent {
 
   colorScheme = 'nightLights';
 
-  constructor() { }
+  intervalo;
+  constructor() {
+
+    this.intervalo = setInterval(() => {
+
+      const newResults = [...this.results];
+
+      for (let i in newResults) {
+        newResults[i].value = Math.round(Math.random() * 500)
+      }
+
+
+      this.results = [...newResults];
+    }, 1500);
+
+  }
 
   onSelect(event) {
     console.log(event);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalo);
   }
 
 }
